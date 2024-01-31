7d32da2c301c7a7716f871671ef23f0540d3e592
rohanbari <rohanbari4@gmail.com>
2024-01-31 11:13:30 +0530

    Initial commit

---
+++ src/main/java/org/rohanbari/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Rohan Bari <rohanbari@outlook.com>
+ */
+
+package org.rohanbari;
+
+public class Main {
+
+    /**
+     * The main method.
+     *
+     * @param args System arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
